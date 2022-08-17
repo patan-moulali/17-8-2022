6850be03f43c05cf111efd62f6976dfb0561b4e3
LENOVO <LENOVO@DESKTOP-JJRNDTI>
2022-08-17 17:45:13 +0530

    Add project files.

---
+++ 17-8-2022/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2nd_day_demo
+{
+    internal class program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int i;
+            float radius;
+            float pi = 3.14f;
+            Console.WriteLine("enter radius");
+            radius = Convert.ToSingle(Console.ReadLine());
+            double area = pi * radius * radius;
+            Console.WriteLine(area);
+        }
+    }
+}
+ 
+
+
+
+
+
+
+
+   
